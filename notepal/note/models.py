f268d9bee505d4972ae3044b64445165cfde74a3
Zion Umoh <zionchrist8@gmail.com>
2023-08-16 08:20:43 +0100

    Updated note file embedding model in note app

--- notepal/note/models.py
+++ notepal/note/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 import uuid
 from pgvector.django import VectorField
-
+from django.contrib.auth.models import User
 # Create note file embedding model
 class NoteFileembedding(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, editable=False, primary_key=True
     )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     name = models.TextField(unique=True)
     created_ts = models.DateTimeField(auto_now_add=True)
     updated_ts = models.DateTimeField(auto_now=True)
     the_file = models.BinaryField()
-    date_published = models.DateTimeField(null=True, blank=True)
-    publisher = models.TextField(null=True, blank=True)
     metadata = models.JSONField(null=True, blank=True)
  
 
