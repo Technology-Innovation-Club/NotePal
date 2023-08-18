d6a4750e4bc016df0e8394cfcd98b1ac37fd888a
Zion Umoh <zionchrist8@gmail.com>
2023-08-18 13:14:32 +0100

    Reassigned note embedding model to chat app

--- notepal/chat/models.py
+++ notepal/chat/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
+from pgvector.django import VectorField
+from note.models import NoteFileembedding
 
 
+class NoteEmbedding(models.Model):
+    id = models.AutoField(primary_key=True)
+    date_created = models.DateField(auto_now_add=True)
+    file_text = models.TextField()
+    file_embedding = models.ForeignKey(NoteFileembedding, on_delete=models.PROTECT)
+    vector = VectorField(dimensions=384, null=True)
+    
 class History(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, editable=False, primary_key=True
