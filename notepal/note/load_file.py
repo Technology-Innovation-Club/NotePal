cb62d3c212c0bd3ab54d5c45cdebed27fa1567fd
Zion Umoh <zionchrist8@gmail.com>
2023-08-18 13:31:25 +0100

    Created function to convert text to vector

--- notepal/note/load_file.py
+++ notepal/note/load_file.py
@@ -3,10 +3,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from django.utils import timezone
 import io
 from numpy import ndarray
+from sentence_transformers import SentenceTransformer
 from .handle_file import upload_docx_file, upload_pdf_file, upload_pptx_file, get_docx_text, get_pptx_text, get_pdf_text
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -17,6 +19,11 @@ def get_chunks(text):
     )
     split_texts = text_splitter.create_documents([text])
     return split_texts
+
+
+def get_vector(text) -> ndarray:
+    vector = model.encode(text)
+    return vector
 
 def handle_upload(file, file_type):
     if file_type == 'pdf':
