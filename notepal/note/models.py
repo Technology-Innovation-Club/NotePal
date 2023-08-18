c16291f99ac19afd1e5ff9026d8f00479f634c87
Zion Umoh <zionchrist8@gmail.com>
2023-08-18 13:16:05 +0100

    Adjusted and reapplied note app migrations

--- notepal/note/models.py
+++ notepal/note/models.py
@@ -1,23 +1,19 @@
 from django.db import models
 import uuid
-from pgvector.django import VectorField
 from django.contrib.auth.models import User
-# Create note file embedding model
+
+# Table to store the users note
 class NoteFileembedding(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, editable=False, primary_key=True
     )
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, default='')
-    name = models.TextField(unique=True)
-    created_ts = models.DateTimeField(auto_now_add=True)
-    updated_ts = models.DateTimeField(auto_now=True)
+    name = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
     the_file = models.BinaryField()
     metadata = models.JSONField(null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default='')
  
-
-class NoteEmbedding(models.Model):
-    id = models.AutoField(primary_key=True)
-    created_ts = models.DateTimeField(auto_now_add=True)
-    text = models.TextField()
-    file_embedding = models.ForeignKey(NoteFileembedding, on_delete=models.PROTECT)
-    vector = VectorField(dimensions=1536, null=True)
+# Table to store the embeddings of the users note
+# add user later 
+# file_owner = models.ForeignKey(User, on_delete=models.CASCADE, default='')
