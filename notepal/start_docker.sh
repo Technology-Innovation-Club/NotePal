42d792f3c4d3dadb2f66c6c1bda9be1b2722939b
Zion Umoh <zionchrist8@gmail.com>
2023-08-19 09:08:11 +0100

    Update start_docker.sh

--- notepal/start_docker.sh
+++ notepal/start_docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 python manage.py migrate
 
+
+docker run -d -p 9000:9000 -p 5434:5432 --env-file=.env notepal_web
+# Set your OpenAI API key here
+
 if [ ! -f /.container_created ]; then
     echo "Setting up..."
 
