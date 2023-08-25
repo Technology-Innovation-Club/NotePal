57d6b946981cd5380b8455f88502996fbcd017dd
Zion Umoh <zionchrist8@gmail.com>
2023-08-25 12:52:36 +0100

    Created chat page

--- notepal/chat/views.py
+++ notepal/chat/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 
 def signup(request):
-    title = "Sign up"
+    title = "| Sign up"
     context = {"title": title}
     return render(request, "signup.html", context)
 
 
 def login(request):
-    title = "Sign in"
+    title = "| Sign in"
     context = {"title": title}
     return render(request, "login.html", context)
 
