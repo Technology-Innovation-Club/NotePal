b31172366a833d3050ce51720272ccc28f00d8b7
Zion Umoh <zionchrist8@gmail.com>
2023-08-26 05:28:38 +0100

    Update notepal user model

--- notepal/users/models.py
+++ notepal/users/models.py
@@ -8,7 +8,7 @@ class NotepalUser(models.Model):
         default=uuid.uuid4, unique=True, editable=False, primary_key=True
     )
     user = models.OneToOneField(
-        User, on_delete=models.PROTECT, related_name="notepaluser"
+        User, on_delete=models.CASCADE, related_name="notepaluser"
     )
     created_ts = models.DateTimeField(auto_now_add=True)
     updated_ts = models.DateTimeField(auto_now=True)
