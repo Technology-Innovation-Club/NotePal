968b19dc52bf24c5c2936742fcb03495312b9a93
Zion Umoh <zionchrist8@gmail.com>
2023-08-26 05:26:45 +0100

    Added signup validation

--- notepal/users/api.py
+++ notepal/users/api.py
@@ -16,7 +16,10 @@ class SignUp(Schema):
 
 @notepal_router.post("/signup")
 def signup(request, signup_details: SignUp = Form(...)):
-    username = signup_details.username
+    if not signup_validate_required_fields(signup_details):
+        return signup_validate_required_fields(signup_details)
+    if not validate_signup_details(signup_details):
+        return validate_signup_details(signup_details)
     password = signup_details.password
     email = signup_details.email
 
@@ -34,12 +37,31 @@ def signup(request, signup_details: SignUp = Form(...)):
     return user.notepaluser.created_ts
 
 
-def email_check(email: str) -> bool:
-    new_email = email.split("@")
-    if new_email[1] == "pau.edu.ng":
-        return True
-    else:
-        return False
+def signup_validate_required_fields(data):
+    errors = {}
+    if data.email == "":
+        errors["email"] = "Email is required"
+    if data.password == "":
+        errors["password"] = "Password is required"
+    if data.repeat_password == "":
+        errors["repeat_password"] = "Repeat password is required"
+    if errors:
+        raise HttpError(422, message=errors)
+    return True
+
+def validate_signup_details(signup_details):
+    errors = {}
+    if not re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', signup_details.email):
+        errors["email"] = "Invalid email address"
+    if User.objects.filter(email=signup_details.email).exists():
+        errors["email"] = "Email is already in use"
+    if signup_details.password != signup_details.repeat_password:
+        errors['password'] = "Passwords do not match"
+    if len(signup_details.password)<8:
+        errors['password'] = "Password is too short. It must be at least 8 characters long"
+    if errors:
+        raise HttpError(422, message=errors)
+    return True
 
 
 # Login can be done through 'accounts/login' and logout through 'accounts/logout'
