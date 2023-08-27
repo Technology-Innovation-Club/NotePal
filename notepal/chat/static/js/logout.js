b20d7af0c208769b8c8e76a2b45691208cffbd21
Zion Umoh <zionchrist8@gmail.com>
2023-08-27 08:11:32 +0100

    Updated logout JS file

--- notepal/chat/static/js/logout.js
+++ notepal/chat/static/js/logout.js
@@ -1,10 +1,11 @@
-// Assuming you have a logout button or link with an ID "logout-button"
-const logoutButton = document.getElementById('logout-button');
+// Assuming you have a logout link with an ID "logout-link"
+const logoutLink = document.getElementById('logout-link');
 
-logoutButton.addEventListener('click', async () => {
+logoutLink.addEventListener('click', async (event) => {
+  event.preventDefault(); // Prevent the default link behavior
+
   try {
     const response = await axios.get('/api/logout'); // Change the URL to your logout endpoint
-    console.log(response.data); // Assuming the response contains logout success data
     // Redirect the user to the login page or any other appropriate page
     window.location.href = '/login'; // Replace with the correct URL
   } catch (error) {
