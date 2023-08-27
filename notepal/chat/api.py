6a25e8e504bb5fd1906f585d3f243de590e3d234
Zion Umoh <zionchrist8@gmail.com>
2023-08-27 08:09:54 +0100

    Updated query endpoint

--- notepal/chat/api.py
+++ notepal/chat/api.py
@@ -12,38 +12,40 @@ chat_router = Router()
 # query chatbot
 @chat_router.post("/query", auth=django_auth)
 def query(request, query: str):
-    user = get_object_or_404(User, username="admin")
-    response = ask_question_stuff(query)
-    if len(response["query_context"]) > 0:
-        user_question = response["user_question"]
-        embedding_context = response["query_context"]
-        llm_algo_used = response["llm_algo_used"]
-        response_to_user = response["response_to_user"]
-        llm_response = response["response"]
-        user_id = user
+    if request.user.is_authenticated:
+        user = get_object_or_404(User, username=request.user.username)
+        response = ask_question_stuff(query)
+        if len(response["query_context"]) > 0:
+            user_question = response["user_question"]
+            embedding_context = response["query_context"]
+            llm_algo_used = response["llm_algo_used"]
+            response_to_user = response["response_to_user"]
+            llm_response = response["response"]
+            user_id = user
 
-        # save to table
-        History.objects.create(
-            user_id=user_id,
-            user_question=user_question,
-            llm_response=llm_response,
-            response_to_user=response_to_user,
-            llm_algo_used=llm_algo_used,
-            embedding_context=embedding_context,
-        )
-        return response["response_to_user"]
-    else:
-        user_question = response["user_question"]
-        llm_algo_used = response["llm_algo_used"]
-        response_to_user = response["response_to_user"]
-        llm_response = response["response"]
-        user_id = user
+            # save to table
+            History.objects.create(
+                user_id=user_id,
+                user_question=user_question,
+                llm_response=llm_response,
+                response_to_user=response_to_user,
+                llm_algo_used=llm_algo_used,
+                embedding_context=embedding_context,
+            )
+            return response["response_to_user"]
+        else:
+            user_question = response["user_question"]
+            llm_algo_used = response["llm_algo_used"]
+            response_to_user = response["response_to_user"]
+            llm_response = response["response"]
+            user_id = user
 
-        History.objects.create(
-            user_id=user_id,
-            user_question=user_question,
-            llm_response=llm_response,
-            response_to_user=response_to_user,
-            llm_algo_used=llm_algo_used,
-        )
-        return response["response_to_user"]
+            History.objects.create(
+                user_id=user_id,
+                user_question=user_question,
+                llm_response=llm_response,
+                response_to_user=response_to_user,
+                llm_algo_used=llm_algo_used,
+            )
+            return response["response_to_user"]
+
