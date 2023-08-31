075cfcc1d8f71bd6387b73f08f63ef02d85d177d
Zion Umoh <zionchrist8@gmail.com>
2023-08-31 12:27:21 +0100

    Created endpoints for users openai api keys

--- notepal/note/api.py
+++ notepal/note/api.py
@@ -9,9 +9,15 @@ from django.shortcuts import get_object_or_404
 from ninja.security import django_auth
 from ninja.errors import HttpError
 from .validate_file import validate_uploaded_file
+from users.models import NotepalUser
+from ninja import Schema
+
 
 note_router = Router()
 
+
+class ApiKeySchema(Schema):
+    api_key: str
 
 # upload a file
 @note_router.post("/upload", auth=django_auth)
@@ -65,6 +71,55 @@ def remove(request, filename: str):
         note = get_object_or_404(NoteFileembedding, name=filename, owner=user)
         note.delete()
         return f"{filename} has been removed"
+    else:
+        error = {}
+        error["error"] = "User not authenticated"
+        raise HttpError(401, message=error)
+
+@note_router.post("/add-api-key", auth=django_auth)
+def add_api_key(request, APIKEY: ApiKeySchema = Form(...)):
+    owner = get_object_or_404(User, email=request.user.email)
+    if request.user.is_authenticated:
+        try:
+            notepal_user = get_object_or_404(NotepalUser,user=owner)
+            notepal_user.api_key = APIKEY.api_key
+            notepal_user.save()
+            return {"message": "API key added successfully"}
+        except NotepalUser.DoesNotExist:
+            return {"message": "NotepalUser does not exist"}
+    else:
+        error = {}
+        error["error"] = "User not authenticated"
+        raise HttpError(401, message=error)
+
+
+@note_router.post("/remove-api-key", auth=django_auth)
+def remove_api_key(request):
+    owner = get_object_or_404(User, email=request.user.email)
+    if request.user.is_authenticated:
+        try:
+            notepal_user = get_object_or_404(NotepalUser, user=owner)
+            notepal_user.api_key = ''
+            notepal_user.save()
+            return {"message": "API key removed successfully"}
+        except NotepalUser.DoesNotExist:
+            return {"message": "NotepalUser does not exist"}
+    else:
+        error = {}
+        error["error"] = "User not authenticated"
+        raise HttpError(401, message=error)
+
+
+@note_router.get("/view-api-key", auth=django_auth)
+def view_api_key(request):
+    owner = get_object_or_404(User, email=request.user.email)
+    if request.user.is_authenticated:
+        try:
+            notepal_user = get_object_or_404(NotepalUser, user=owner)
+            api_key = notepal_user.api_key
+            return {"api_key": api_key}
+        except NotepalUser.DoesNotExist:
+            return {"message": "NotepalUser does not exist"}
     else:
         error = {}
         error["error"] = "User not authenticated"
