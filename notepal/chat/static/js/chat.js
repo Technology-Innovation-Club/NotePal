9973e5e4a117f3d769da53dd6c9f978f4549a40e
Zion Umoh <zionchrist8@gmail.com>
2023-10-05 16:16:52 +0100

    Migrated JS script to handle upload

--- notepal/chat/static/js/chat.js
+++ notepal/chat/static/js/chat.js
@@ -1,8 +1,13 @@
-// Get the necessary DOM elements
 const questionTextarea = document.querySelector('#question-area');
-const chatList = document.querySelector('#chat-list');
+var chatList = document.querySelector('#chat-list');
 const sendButton = document.querySelector('#send-button');
 const loadingSpinner = document.querySelector('#send-loading');
+const uploadButton = document.getElementById('upload-button');
+const spinningElement = document.querySelector('.animate-spin');
+const alertWrongFile = document.getElementById('alertWrongFile');
+const alertExistingFile = document.getElementById('alertExistingFile');
+
+// CHAT HANDLING
 
 function convertMarkdown(content) {
   var converter = new showdown.Converter();
@@ -11,6 +16,8 @@ function convertMarkdown(content) {
   
   return html;
 }
+
+
 
 
 
@@ -87,6 +94,7 @@ sendButton.addEventListener('click', async () => {
 // Function to submit the question
 async function submitQuestion() {
   const question = document.getElementById('question-area').value;
+  console.log('the question is: '+question)
   if (question) {
     questionTextarea.value = '';
     loadingSpinner.classList.remove('hidden');
@@ -109,10 +117,10 @@ async function submitQuestion() {
     `;
     chatList.appendChild(questionBubble);
     // Scroll to the latest question-answer pair
-    chatList.scrollTop = chatList.scrollHeight;
+    // chatList.scrollTop = chatList.scrollHeight;
 
     // Create and display the loading spinner
-    chatList.scrollTop = chatList.scrollHeight;
+    // chatList.scrollTop = chatList.scrollHeight;
 
     try {
       await sendQuestion(question);
@@ -121,13 +129,120 @@ async function submitQuestion() {
       loadingSpinner.classList.add('hidden');
 
       // Scroll to the latest question-answer pair
-      chatList.scrollTop = chatList.scrollHeight;
+      // chatList.scrollTop = chatList.scrollHeight;
     } catch (error) {
       console.error('Error sending question:', error);
       // Handle the error here, e.g., by displaying an error message to the user.
     }
   }
 }
+
+// FILE UPLOAD HANDLING
+uploadButton.addEventListener('click', async () => {
+  // Get the selected file from the file input
+  const fileInput = document.getElementById('file-input');
+  const file = fileInput.files[0];
+  
+  const formData = new FormData();
+  formData.append('file', file);
+  
+  const csrftoken = getCookie('csrftoken'); 
+  const headers = {
+    'X-CSRFToken': csrftoken,
+  };
+
+  try {
+    // Show the spinning element while uploading
+    spinningElement.classList.remove('hidden');
+    
+    const response = await axios.post('/api/note/upload', formData, { headers });
+    console.log(response.data); // Handle success here
+
+    // Hide the spinning element after upload is complete
+    spinningElement.classList.add('hidden');
+
+    // Create a chat bubble for the uploaded file
+    const uploadedFileBubble = createUploadedFileBubble(file);
+
+    // Append the chat bubble to the chat list
+    chatList.appendChild(uploadedFileBubble);
+    
+    // Refresh the page when upload is successful
+    // window.location.reload();
+    const closeButton = document.querySelector('.hs-dropdown-toggle'); // Select the button using its class
+    // Trigger the click event on the button
+    if (closeButton) {
+      closeButton.click();
+    }
+  } catch (error) {
+    console.error(error);
+    console.log("this is the error status: "+error.response.status);
+
+    if (error.response && error.response.status === 401) {
+        console.error('User not authenticated');
+        // Redirect the user to the login page
+        window.location.href = '/login'; 
+    } else if (error.response && error.response.status === 400) {
+        alertWrongFile.classList.remove('hidden');
+    }else if (error.response && error.response.status === 409) {
+      console.log('Displaying existing file alert');
+        alertExistingFile.classList.remove('hidden');
+        }
+    }
+    // Handle other error cases here
+  }
+);
+
+function createUploadedFileBubble(file) {
+  const uploadedFileBubble = document.createElement('li');
+  uploadedFileBubble.innerHTML = `
+    <!-- Chat Bubble -->
+    <div class="max-w-4xl px-4 sm:px-6 lg:px-8 mx-auto">
+      <div class="max-w-2xl flex gap-x-2 sm:gap-x-4">
+        <span class="flex-shrink-0 inline-flex items-center justify-center h-[2.375rem] w-[2.375rem] rounded-full bg-gray-600">
+          <span class="text-sm font-medium text-white leading-none">AZ</span>
+        </span>
+
+        <div class="grow mt-2 space-y-3">
+          <p class="text-gray-800 dark:text-gray-200">${removeFileExtension(file.name)} uploaded</p>
+          <ul class="flex flex-col justify-end text-start -space-y-px">
+                <li class="flex items-center gap-x-2 p-3 text-sm bg-white border text-gray-800 first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-slate-900 dark:border-gray-700 dark:text-gray-200">
+                  <div class="w-full flex justify-between truncate">
+                    <span class="mr-3 flex-1 w-0 truncate">
+                      ${file.name}
+                    </span>
+                    <a class="flex items-center gap-x-2 text-gray-500 hover:text-blue-500 whitespace-nowrap" href="javascript:;">
+                      <svg class="flex-shrink-0 w-3 h-3" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
+                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                      </svg>
+                      Download
+                    </a>
+                  </div>
+                </li>
+              </ul>
+        </div>
+      </div>
+    </div>
+    <!-- End Chat Bubble -->
+  `;
+  return uploadedFileBubble;
+}
+
+function removeFileExtension(fileName) {
+  // Split the file name by dot (.)
+  const parts = fileName.split('.');
+
+  // Check if there's more than one part (i.e., there's an extension)
+  if (parts.length > 1) {
+    // Remove the last part (the extension) and join the remaining parts
+    return parts.slice(0, -1).join('.');
+  }
+
+  // If there's no extension, return the original name
+  return fileName;
+}
+
 
 
 // Function to get a cookie by name
