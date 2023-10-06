7a4413a40d2a8ddc0e0762c95ce2d58ed56ffef9
Zion Umoh <zionchrist8@gmail.com>
2023-10-06 15:03:22 +0100

    Created functionality to Retrieve the users history

--- notepal/chat/static/js/chat.js
+++ notepal/chat/static/js/chat.js
@@ -200,11 +200,11 @@ function createUploadedFileBubble(file) {
     <div class="max-w-4xl px-4 sm:px-6 lg:px-8 mx-auto">
       <div class="max-w-2xl flex gap-x-2 sm:gap-x-4">
         <span class="flex-shrink-0 inline-flex items-center justify-center h-[2.375rem] w-[2.375rem] rounded-full bg-gray-600">
-          <span class="text-sm font-medium text-white leading-none">AZ</span>
+          <span class="text-sm font-medium text-white leading-none">N</span>
         </span>
 
         <div class="grow mt-2 space-y-3">
-          <p class="text-gray-800 dark:text-gray-200">${removeFileExtension(file.name)} uploaded</p>
+          <p class="text-gray-800 dark:text-gray-200">New note uploaded</p>
           <ul class="flex flex-col justify-end text-start -space-y-px">
                 <li class="flex items-center gap-x-2 p-3 text-sm bg-white border text-gray-800 first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-slate-900 dark:border-gray-700 dark:text-gray-200">
                   <div class="w-full flex justify-between truncate">
